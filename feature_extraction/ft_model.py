751b487ae94efa0fc156493cd441e7f03dccfed6
Sherlock <sherlockliao01@gmail.com>
2017-09-23 20:25:26 +0800

    change to lstm to get better result

--- feature_extraction/ft_model.py
+++ feature_extraction/ft_model.py
@@ -22,7 +22,7 @@ class feature_model(nn.Module):
         self.project = nn.Sequential(nn.Linear(in_feature, embed_dim))
         # nn.BatchNorm1d(embed_dim, momentum=0.01))
         self.word2vec = nn.Embedding(vocab_size, embed_dim, padding_idx=PAD)
-        self.rnn = nn.GRU(
+        self.rnn = nn.LSTM(
             input_size=embed_dim,
             hidden_size=hidden_size,
             num_layers=num_layers,
@@ -32,8 +32,8 @@ class feature_model(nn.Module):
         self.init_weight()
 
     def init_weight(self):
-        # nn.init.normal(self.project[0].weight, 0, 0.1)
-        # nn.init.constant(self.project[0].bias, 0)
+        nn.init.normal(self.project[0].weight, 0, 0.1)
+        nn.init.constant(self.project[0].bias, 0)
         nn.init.uniform(self.word2vec.weight, -0.1, 0.1)
         nn.init.uniform(self.classifer.weight, -0.1, 0.1)
         nn.init.constant(self.classifer.bias, 0)
@@ -44,21 +44,11 @@ class feature_model(nn.Module):
         label: batch x length
         seq_len: label length list
         """
-        # print(self.project[0].weight)
-        # input("")
         proj_ft = self.project(ft)  # b x embed
-        # print(proj_ft)
-        # input("")
         embedding = self.word2vec(label)  # b x le x embed
-        # print(embedding)
-        # input("")
         combine_input = torch.cat((proj_ft.unsqueeze(1), embedding), 1)
         combine_input = combine_input.permute(1, 0, 2)  # (len + 1) x b x embed
         combine_input = pack_padded_sequence(combine_input, seq_len)
         out, _ = self.rnn(combine_input)
-        # print(out)
-        # input("")
         output = self.classifer(out[0])
-        # print(output)
-        # input("")
         return output
